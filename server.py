4d44b21837e83796f72f01ad2be71d00bf6c6c28
zming <email@zming.info>
2021-06-22 00:18:22 +0800

    hot fix

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-from ProxyIP.webapi import app
+from sanic import Sanic
 from ProxyIP.config import SANIC_HOST, SANIC_PORT, SANIC_ACCESS_LOG
 
 
+app = Sanic('proxy ip')
 # start web app
 app.run(host=SANIC_HOST, port=SANIC_PORT, access_log=SANIC_ACCESS_LOG)
